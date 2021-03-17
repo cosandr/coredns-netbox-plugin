b4638cd9f2745709640f0e3bdaa8bbd87dc31f16
Andrei Costescu <andrei@costescu.no>
2021-03-17 15:25:08 +0100

    Query rework
    Change endpoint to /api/dcim/devices/ and read the primary IP address of the device

--- query.go
+++ query.go
@@ -16,7 +16,6 @@ package netbox
 import (
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"net/http"
 	"strings"
 	"time"
@@ -25,9 +24,13 @@ import (
 	"github.com/imkira/go-ttlmap"
 )
 
+type IP struct {
+	Address string `json:"address"`
+}
+
 type Record struct {
-	Address  string `json:"address"`
-	HostName string `json:"dns_name,omitempty"`
+	PrimaryIP4 IP     `json:"primary_ip4"`
+	Name       string `json:"name,omitempty"`
 }
 
 type RecordsList struct {
@@ -35,52 +38,52 @@ type RecordsList struct {
 }
 
 var localCache = ttlmap.New(nil)
+var client = &http.Client{Timeout: 5 * time.Second}
 
 func query(url, token, dns_name string, duration time.Duration) string {
 	item, err := localCache.Get(dns_name)
 	if err == nil {
-		clog.Debug(fmt.Sprintf("Found in local cache %s", dns_name))
+		clog.Debugf("found in local cache %s", dns_name)
 		return item.Value().(string)
-	} else {
-		records := RecordsList{}
-		client := &http.Client{}
-		var resp *http.Response
-		clog.Debug("Querying ", fmt.Sprintf("%s/?dns_name=%s", url, dns_name))
-		req, err := http.NewRequest("GET", fmt.Sprintf("%s/?dns_name=%s", url, dns_name), nil)
-		req.Header.Set("Authorization", fmt.Sprintf("Token %s", token))
-
-		for i := 1; i <= 10; i++ {
-			resp, err = client.Do(req)
-
-			if err != nil {
-				clog.Fatalf("HTTP Error %v", err)
-			}
-
-			if resp.StatusCode == http.StatusOK {
-				break
-			}
-
-			time.Sleep(1 * time.Second)
-		}
-		// TODO: check that we got status code 200
-		body, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			clog.Fatalf("Error reading body %v", err)
-		}
-
-		jsonAns := string(body)
-		err = json.Unmarshal([]byte(jsonAns), &records)
-		if err != nil {
-			clog.Fatalf("could not unmarshal response %v", err)
-		}
-
-		if len(records.Records) == 0 {
-			clog.Info("Recored not found in", jsonAns)
-			return ""
-		}
-
-		ip_address := strings.Split(records.Records[0].Address, "/")[0]
-		localCache.Set(dns_name, ttlmap.NewItem(ip_address, ttlmap.WithTTL(duration)), nil)
-		return ip_address
 	}
+	records := RecordsList{}
+	var resp *http.Response
+	req, err := http.NewRequest("GET", fmt.Sprintf("%s/api/dcim/devices/", url), nil)
+	if err != nil {
+		clog.Errorf("cannot create request %v", err)
+		return ""
+	}
+	q := req.URL.Query()
+	q.Add("name", dns_name)
+	req.URL.RawQuery = q.Encode()
+	req.Header.Set("Authorization", fmt.Sprintf("Token %s", token))
+
+	clog.Infof("GET %s", req.URL.String())
+	resp, err = client.Do(req)
+	if err != nil {
+		clog.Errorf("HTTP Error %v", err)
+		return ""
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		clog.Errorf("invalid response code %d", resp.StatusCode)
+		return ""
+	}
+	defer resp.Body.Close()
+
+	err = json.NewDecoder(resp.Body).Decode(&records)
+	if err != nil {
+		clog.Errorf("could not unmarshal response %v", err)
+		return ""
+	}
+
+	if len(records.Records) == 0 {
+		clog.Info("record not found")
+		return ""
+	}
+
+	ip_address := strings.Split(records.Records[0].PrimaryIP4.Address, "/")[0]
+	clog.Infof("record found %s", ip_address)
+	localCache.Set(dns_name, ttlmap.NewItem(ip_address, ttlmap.WithTTL(duration)), nil)
+	return ip_address
 }
